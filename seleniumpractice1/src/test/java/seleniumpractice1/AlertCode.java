a0d0a026c2a69a7c59e96377cf400cf127af5ee2
hp <hp@DESKTOP-30OBHVG>
2025-06-18 17:58:05 +0530

    alertcode has done

--- seleniumpractice1/src/test/java/seleniumpractice1/AlertCode.java
+++ seleniumpractice1/src/test/java/seleniumpractice1/AlertCode.java
@@ -1,9 +1,68 @@
 package seleniumpractice1;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class AlertCode {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+		
+		driver.manage().window().maximize();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		
+		driver.switchTo().alert().accept();
+		
+		
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+		
+		driver.switchTo().alert().dismiss();
+		
+		
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+		
+		Alert alert = driver.switchTo().alert();
+		
+		alert.sendKeys("Priyanka");
+		
+		alert.accept();
+		
+		String compare = driver.findElement(By.cssSelector("#result")).getText();
+		
+		if(compare.contains("Priyanka"))
+		{
+			TakesScreenshot ts = (TakesScreenshot) driver;
+			File screenshotFile = ts.getScreenshotAs(OutputType.FILE);
+			String screenshotPath = "C:\\Priya\\seleniumpractice1\\ss1file.png";
+
+            // Copy the file to the desired location
+            FileUtils.copyFile(screenshotFile, new File(screenshotPath));
+
+            System.out.println("Screenshot saved to: " + screenshotPath);
+		}
+		else
+		{
+			System.out.println("Test Fail");
+		}
+		
+		
 
 	}
 
